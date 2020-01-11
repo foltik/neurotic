f9d2534f02d84ab430d09367ab6fc59fca3172f1
Jack Foltz <jack@foltz.io>
2020-01-10 21:23:37 -0500

    Add hsv <-> rgb conversions

--- lib.js
+++ lib.js
@@ -30,3 +30,48 @@ const wit = (pre, fn) => {
 };
 
 const at = (x, y, fn) => wit(() => translate(x, y), fn);
+
+const rgb = (h, s, v) => {
+    let r, g, b;
+
+    const i = Math.floor(h * 6);
+    const f = h * 6 - i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+    }
+
+    return [r * 255, g * 255, b * 255];
+};
+
+function hsv(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, v = max;
+
+    const d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0;
+    } else {
+        switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
+    }
+
+    return [h, s, v];
+}
