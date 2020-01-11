7ecefc65475aaded0a530feba4aa614459768c78
Jack Foltz <jack@foltz.io>
2020-01-10 21:23:21 -0500

    Add arange

--- lib.js
+++ lib.js
@@ -1,5 +1,21 @@
 const range = x => [...Array(x)].map((_, i) => i);
 
+function arange(i, j, delta) {
+    if (arguments.length === 1) {
+        return arange(0, i, 1);
+    } else if (arguments.length === 2) {
+        return arange(i, j, 1);
+    }
+
+    let result = [];
+    let n = 0;
+    while (i < j) {
+        result[n++] = i;
+        i += delta;
+    }
+    return result;
+};
+
 const widget = fn => (...args) => {
     push();
     fn(...args);
