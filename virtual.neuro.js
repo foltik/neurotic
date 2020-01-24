024a32ef51bdbbbac78604ec86616951c1cf394a
Jack Foltz <jack@foltz.io>
2020-01-24 00:26:40 -0500

    Add virtual rotation

--- virtual.neuro.js
+++ virtual.neuro.js
@@ -3,7 +3,9 @@ neuro_setup(() => {
     let canvas = createCanvas(800, 800);
 });
 
-function virtual(color) {
+function virtual(color, t) {
+    const [t0, t1, t2, t3] = [t * 0.01, t * 0.04, t * 0.1, t * 0.5];
+
     const [hue, sat, val] = hsv(...color);
     strokeCap(PROJECT);
 
@@ -12,15 +14,16 @@ function virtual(color) {
 
     const circ = n => range(0, 2*PI, 2*PI / n);
     const poly = (n, r) => circ(n).amap(t => rectangular(r, t));
-    const poly2 = (n, r) => circ(n).amap(t => rectangular(r, t + PI));
+    const polyshift = (n, r, shift = PI) => circ(n).amap(t => rectangular(r, t + shift));
+    const polyf = (n, r, f) => circ(n).amap(t => rectangular(r, f(t)));
 
     // Outer Circles
     outline(2);
-    arc(0, 0, 630, 630, 0, 1);
-    arc(0, 0, 630, 630, 1.5, 2);
-    arc(0, 0, 630, 630, 2.5, 3.8);
-    arc(0, 0, 630, 630, 4.3, 4.6);
-    arc(0, 0, 630, 630, 5.1, 5.8);
+    arc(0, 0, 630, 630, t0, 1 + t0);
+    arc(0, 0, 630, 630, 1.5 + t0, 2 + t0);
+    arc(0, 0, 630, 630, 2.5 + t0, 3.8 + t0);
+    arc(0, 0, 630, 630, 4.3 + t0, 4.6 + t0);
+    arc(0, 0, 630, 630, 5.1 + t0, 5.8 + t0);
     circle(0, 0, 580);
     circle(0, 0, 460);
 
@@ -32,23 +35,23 @@ function virtual(color) {
     outline(2);
     const radial = ninterp([0, 0], [0, 0.1], [val, 0.25], [0, 0.5], [0, 0.6], [val, 0.75], [0, 1]);
     zip(rad0, rad1, ts).each(([[x0, y0], [x1, y1], t]) => {
-        stroke(...rgb(hue, sat, radial(t)));
+        stroke(...rgb(hue, sat, radial(t + t0)));
         line(x0, y0, x1, y1);
     });
 
     // Triangle Lines
     outline(4);
-    poly2(3, 230).each(([x, y]) =>
+    polyf(3, 230, t => t - t1 + PI).each(([x, y]) =>
         line(x, y, 0, 0));
 
     // Triangle
     outline(2);
     fill(0);
-    const tri = poly(3, 300);
+    const tri = polyf(3, 300, t => t - t1);
     triangle(...tri.flat());
 
     outline(0.5);
-    triangle(...poly2(3, 150).flat());
+    triangle(...polyf(3, 150, t => t - t1 + PI).flat());
 
     outline(2);
     fill(0);
@@ -111,24 +114,24 @@ function virtual(color) {
 
     fill(0);
 
-    poly2(3, 230).each(([x, y]) =>
+    // Triangle Line Circles
+    polyf(3, 230, t => t - t1 + PI).each(([x, y]) =>
         circle(x, y, 20));
 
     // Center Circles
     outline(2);
-    circ(12).zip_next().each(([t0, t1], i) => even(i) && arc(0, 0, 220, 220, t0, t1));
-    circ(12).zip_next().each(([t0, t1], i) => odd(i) && arc(0, 0, 210, 210, t0, t1));
+    circ(12).zip_next().each(([a0, a1], i) => even(i) && arc(0, 0, 220, 220, a0 + t1, a1 + t1));
+    circ(12).zip_next().each(([a0, a1], i) => odd(i) && arc(0, 0, 210, 210, a0 + t1, a1 + t1));
     zip(poly(50, 85), poly(50, 70)).each(([[x0, y0], [x1, y1]]) => {
         line(x0, y0, x1, y1);
     });
 
     outline(12);
-    circ(16).zip_next().each(([t0, t1], i) => even(i) && arc(0, 0, 100, 100, t0, t1));
+    circ(16).zip_next().each(([a0, a1], i) => even(i) && arc(0, 0, 100, 100, a0 + t1, a1 + t1));
 
     // Floaty circle
     outline(1);
-    const ft = 0;
-    const [fx, fy] = [200 * cos(ft), 200 * sin(ft)];
+    const [fx, fy] = [200 * cos(t3 + 0.2), 200 * sin(t3)];
     fill(...color);
     circle(fx, fy, 40);
     fill(0);
@@ -138,9 +141,11 @@ function virtual(color) {
 }
 
 neuro_draw(() => {
+    const t = neuro_get('t', 0);
     background(0);
 
-    scale(0.8);
     translate(400, 400);
-    virtual([0, 192, 255]);
+    virtual([0, 192, 255], t);
+
+    neuro_set('t', t + amp.getLevel());
 });
